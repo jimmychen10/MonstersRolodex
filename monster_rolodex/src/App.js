a1fcadfb593e4dd8f172ea95f88f5087223144eb
jimmychen10 <jimmychen1996@yahoo.com>
2022-08-12 13:57:12 -0700

    added api fetch

--- monster_rolodex/src/App.js
+++ monster_rolodex/src/App.js
@@ -4,27 +4,52 @@ import React, { Component } from 'react'
 
 export default class App extends Component {
   constructor(){
+    super();
     this.state ={
-      
+      monsters:[
+
+      ]
     }
   }
   componentDidMount(){
     fetch("https://jsonplaceholder.typicode.com/users")
     // .then(()=> this.state = response.json)
-    .then((res) => 
+    .then((res) =>res.json())
+    .then((data) => 
       this.setState((state,prop)=> {
         return{
-          res
+          monsters: data
         }
-      }) 
-    )
-    console.log(this.state)
+      },
+      () => {
+        console.log(this.state)
+      } 
+    ))
   }
 
   render() {
     
     return (
-      <div>{this.state.map((key, item)=>item)}</div>
+      <div>
+
+        <input 
+          type="search" 
+          placeholder='search monster'
+          onChange={(event)=>{
+            const filteredMonsters = this.state.monsters.filter((monster)=>{
+             return monster.name.include(event.target.value)
+            })
+          }}
+
+        />
+        {this.state.monsters.map((monster)=>
+          <div key= {monster.id}>
+            <h2>{monster.name}</h2>
+
+          </div>
+          
+        )}
+      </div>
     )
   }
 }
